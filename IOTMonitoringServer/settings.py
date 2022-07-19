fc17bef58269c6f7ed3d72a3b5f960ca14fe0ec2
wilson-bg <w.beltran@uniandes.edu.co>
2022-07-18 21:51:44 -0500

    update ip

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.86.230.90"
+MQTT_HOST = "54.234.200.36"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
