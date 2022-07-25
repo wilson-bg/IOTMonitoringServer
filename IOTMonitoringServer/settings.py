9b40dd90ad94ae507fd77dd65f96eb000e7b1f93
Wilson-bg <wbeltran@millionandup.com>
2022-07-24 20:31:21 -0500

    update ip

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "34.226.150.106"]
+ALLOWED_HOSTS = ["localhost", "3.95.150.123"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.173.146.117",  # Dirección IP de la base de datos
+        "HOST": "54.87.135.13",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.146.246.3"
+MQTT_HOST = "3.95.155.169"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
